8a2bfbf1f3a568b11acf613a256080ff2f6d9475
yongjae5717 <dydwo5717@naver.com>
2022-09-08 19:48:59 +0900

    apply Identity strategy

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,26 +1,81 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    // enum타입이 db에는 없기때문에 Enumerated가 필요
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    // 날짜타입을 이용하기 위함
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    //db의 큰 컨텐츠를 넣고싶을때 사용
+    @Lob
+    private String description;
+
+    public Member() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getUsername() {
+        return username;
     }
 
-    public String getName() {
-        return name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -16,28 +16,9 @@ public class jpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("hello");
-//
-//            em.persist(member);
-//
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("helloJPA");
-//            em.remove(findMember);
-
-//            //멤버 객체를 대상으로 쿼리를 구성 (JPQL)
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .getResultList();
-//            for (Member member : result) {
-//                System.out.println("member.getName() = " + member.getName());
-//            }
-
-            // 비영속
             Member member = new Member();
-            member.setId(10L);
-            member.setName("회원1");
-            //객체를 저장한 상태(영속)
+            member.setUsername("C");
+
             em.persist(member);
 
             tx.commit();
