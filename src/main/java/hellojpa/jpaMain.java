8741d003c6d737071130f8a91b99e8faeb9f948e
yongjae5717 <dydwo5717@naver.com>
2022-09-08 16:32:11 +0900

    using JPQAL

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class jpaMain {
     public static void main(String[] args) {
@@ -15,8 +16,22 @@ public class jpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("helloJPA");
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("hello");
+//
+//            em.persist(member);
+//
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("helloJPA");
+//            em.remove(findMember);
+
+            //멤버 객체를 대상으로 쿼리를 구성 (JPQAL)
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e){
