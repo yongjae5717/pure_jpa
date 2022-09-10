f21c8b42ddbc12de27718fcdfb236a5602dc79ba
yongjae5717 <dydwo5717@naver.com>
2022-09-10 23:32:23 +0900

    using JOIN TABLE STRATEGY

--- src/main/java/hellojpa/Album.java
+++ src/main/java/hellojpa/Album.java
@@ -1,8 +1,11 @@
 package hellojpa;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("ALBUM")
 public class Album extends Item{
     private String artist;
 }
--- src/main/java/hellojpa/Book.java
+++ src/main/java/hellojpa/Book.java
@@ -1,8 +1,10 @@
 package hellojpa;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("BOOk") // default: class명
 public class Book extends Item{
     private String author;
 
