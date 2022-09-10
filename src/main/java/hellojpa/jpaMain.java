845cc1796dd58dc0b68e2c1aacee7462b056359d
yongjae5717 <dydwo5717@naver.com>
2022-09-10 16:16:19 +0900

    양방향 연관관계 (순수 객체 상태를 고려해서 항상 양쪽에 값을 설정하자

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -25,22 +25,24 @@ public class jpaMain {
             //멤버 저장
             Member member = new Member();
             member.setUsername("memberA");
-            member.setTeam(team);
+            member.changeTeam(team); //양방향으로 변경한 값이 저장되도록 만든 기능
             em.persist(member);
 
-            em.flush();
-            em.clear();
+//            em.flush();
+//            em.clear();
 
             //멤버 조회
             Member findMember = em.find(Member.class, member.getId());
 //            Team findTeam = em.find(Team.class, findMember.getTeam());
 
+
+            System.out.println("=================");
             //팀 멤버 조회
             List<Member> members = findMember.getTeam().getMembers();
             for (Member m : members) {
                 System.out.println("m.getUsername() = " + m.getUsername());
             }
-
+            System.out.println("=================");
 //            //팀 변경
 //            Team newTeam = em.find(Team.class, 100L);
 //            findMember.setTeam(newTeam);
