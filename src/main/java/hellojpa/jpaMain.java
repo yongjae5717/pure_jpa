55e4f2fe427e78b6fef43b2716e54373adf82258
yongjae5717 <dydwo5717@naver.com>
2022-09-10 15:41:52 +0900

    양방향 연관관계 적용

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 
 public class jpaMain {
@@ -27,9 +28,22 @@ public class jpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             //멤버 조회
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = em.find(Team.class, findMember.getTeam());
+//            Team findTeam = em.find(Team.class, findMember.getTeam());
+
+            //팀 멤버 조회
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
+
+//            //팀 변경
+//            Team newTeam = em.find(Team.class, 100L);
+//            findMember.setTeam(newTeam);
 
             tx.commit();
         } catch (Exception e){
