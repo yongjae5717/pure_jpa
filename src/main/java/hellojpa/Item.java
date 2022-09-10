c8804cce6d0fca2206866ce4bfc2a836451d4d9e
yongjae5717 <dydwo5717@naver.com>
2022-09-10 23:26:11 +0900

    superclass (JOIN) test

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -7,13 +7,34 @@ import javax.persistence.*;
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Item {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "item_id", nullable = false)
+    @GeneratedValue
     private Long id;
 
     private String name;
 
     private int price;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -17,35 +17,21 @@ public class jpaMain {
         tx.begin();
 
         try {
-            //팀 저장
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Movie movie = new Movie();
+            movie.setDirector("A");
+            movie.setActor("B");
+            movie.setName("바람과 함께 사라지다");
+            movie.setPrice(10000);
+            em.persist(movie);
 
-            //멤버 저장
-            Member member = new Member();
-            member.setUsername("memberA");
-            member.changeTeam(team); //양방향으로 변경한 값이 저장되도록 만든 기능
-            em.persist(member);
+            //1차 캐시 초기화
+            em.flush();
+            em.clear();
 
-//            em.flush();
-//            em.clear();
-
-            //멤버 조회
-            Member findMember = em.find(Member.class, member.getId());
-//            Team findTeam = em.find(Team.class, findMember.getTeam());
-
-
-            System.out.println("=================");
-            //팀 멤버 조회
-            List<Member> members = findMember.getTeam().getMembers();
-            for (Member m : members) {
-                System.out.println("m.getUsername() = " + m.getUsername());
-            }
-            System.out.println("=================");
-//            //팀 변경
-//            Team newTeam = em.find(Team.class, 100L);
-//            findMember.setTeam(newTeam);
+            System.out.println("=============");
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
+            System.out.println("=============");
 
             tx.commit();
         } catch (Exception e){
